5cf6be2ac7e849a56adcc59d3c556ebcbc1cda3c
shilohchiu <129954689+shilohchiu@users.noreply.github.com>
2023-11-30 12:11:21 -0500

    Created individual supporting files
    - main.py: where the game will actually run;
    imported files:
    - constants.py: (currently contains an empty pizza string and some toppings we brainstormed)
    - functions.py: added a new argument to the replace topping function and added the piece of code that actually modifies the new pizza-output-string
    - readme.txt: the introduction to our project
    - requirements.txt: where we will keep track of our imported modules/version of python needed for our env

---
+++ functions.py
@@ -0,0 +1,30 @@
+"""
+All the major functions our game relies on.
+"""
+
+def replace_blank(topping, pizza):
+  lst = []
+  # reads through the pizza and replaces 
+  # each underscore with the topping
+  for character in pizza:
+    lst.append(character)
+
+  for c in range(len(lst)):
+    if lst[c] == '_' and lst[c - 1] == '(' and lst[c+1] == ')':
+      lst[c] = topping
+
+    good_lst = replace_blank('topping')
+    empty = ''
+    p = 0
+    for g in good_lst:
+        empty += good_lst[p]
+        p += 1
+
+  return empty
+
+
+    
+# print(empty)
+
+
+# def make_pizza():
---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+PIZZATRON 3000
+"""
+
+import constants
+import functions
+import random
+
+if __name__ == "__main__":
+    # initializes the list of toppings (based on dictionary
+    # toppings); changes as more toppings are available to 
+    # the user
+    toppings_list = []
+    constants.CHEESEPIZZA
