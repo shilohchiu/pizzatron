9e2e01855ea7d2aaa679a71373aabbce86d93a5b
shilohchiu <129954689+shilohchiu@users.noreply.github.com>
2023-12-01 11:34:23 -0500

    Fixed the generate pizza function
    no blanks appear; also adjusted the probability of changing a single underscore to scale as less and less underscores are available to be changed

--- functions.py
+++ functions.py
@@ -72,38 +72,35 @@ def generate_pizza(unlocked_toppings, pizza_template):
     generated_pizza.append(character)
 
   for i, topping in enumerate(picked_toppings):
-    underscore_id = 1
+
     for c in range(len(generated_pizza)):
       
       if generated_pizza[c] == '_' and \
       generated_pizza[c - 1] == '(' \
       and generated_pizza[c+1] == ')':
-        if len(picked_toppings) == 1 or i+1 == len(picked_toppings):
-          choice = 1
+        if i == (len(picked_toppings)-1):
+          generated_pizza[c] = topping
         else:
           choice = random.random()
-
-        
-
-        if choice >= 0.5 or underscore_id == 1:
-          generated_pizza[c] = topping# the current iteration of the topping
+          if choice >= 1 / len(picked_toppings) * (i+1):
+            generated_pizza[c] = topping# the current iteration of the topping
       # if it's the last topping, add one
       # the computer will check the user 
       # input to see if it's in the new list
-        underscore_id += 1
-    new_pizza = ''
-    p = 0
-    for _ in generated_pizza:
-        new_pizza += generated_pizza[p]
-        p += 1
+    
+  new_pizza = ''
+  p = 0
+  for _ in generated_pizza:
+      new_pizza += generated_pizza[p]
+      p += 1
 
     # check and see if the list of toppings is
     # actually in the generated pizza
-    print(picked_toppings)
-    for i, topping in enumerate(picked_toppings):
-      if topping not in generated_pizza:
-        picked_toppings.pop(i)
-    print(picked_toppings)
+  print(picked_toppings)
+  for i, topping in enumerate(picked_toppings):
+    if topping not in generated_pizza:
+      picked_toppings.pop(i)
+  print(picked_toppings)
 
   return new_pizza
 
