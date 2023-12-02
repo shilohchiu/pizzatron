78ce3fa9021f86110cbfc0b7a9078d7fd32cb02e
shilohchiu <129954689+shilohchiu@users.noreply.github.com>
2023-12-02 13:38:35 -0500

    Updated main

--- pizzatron/main.py
+++ pizzatron/main.py
@@ -10,9 +10,11 @@ if __name__ == "__main__":
    # initializes the list of toppings (based on dictionary
    # toppings); changes as more toppings are available to 
    # the user
+   fun.fun_type("----PIZZATRON: CS1210 FINAL PROJECT-----\n")
+   fun.fun_type("Created by: Shiloh Chiu and Alex Boswell\n")
    while True:
       try:
-         name = input("Welcome! What's your name?\n")
+         name = input("Hello! What's your name?\n")
          break
       except ValueError:
          fun.fun_type("That's not a valid name!\n")
@@ -21,10 +23,11 @@ if __name__ == "__main__":
    while True:
       answer = input(f"Are you ready to make some pizzas, {name}? (y/n)\n")
       if answer.upper() == 'Y':
-         fun.fun_type("Great! Let's get started.\n")
+         fun.fun_type(f"Great! Love your enthusiasm, {name}. Let's get started.\n")
          break
       elif answer.upper() == 'N':
-         fun.fun_type("Too bad! Let's get started.\n")
+         fun.fun_type(f"Too bad! You're going to learn to make some"\
+                      " pizzas anyways, {name}. Let's get started.\n")
          break
       else:
          fun.fun_type("Please input y or n as answers! \n")
@@ -34,3 +37,6 @@ if __name__ == "__main__":
                "to put all the toppings on the pizza within a set amount of "\
                "time. The more pizzas you make, the more complicated the pizzas "\
                "get! ")
+   fun.fun_type("At the beginning of each level, you'll be shown a key of the toppings and what each topping is represented as.\n")
+   fun.fun_type("At the beginning of each round, the computer will show you a pizza.\n")
+   fun.fun_type("To recreate the pizza, just type the ")
